0e0546c6f4131a25607caa1f7775a67283dade03
Suyog-Dhakal <suyogdhakal8@gmail.com>
2022-06-14 08:45:07 +0545

    .

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,15 @@
+const { network } = require("hardhat")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+}
+
+//15;1
